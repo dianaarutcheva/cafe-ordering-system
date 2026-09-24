093f51adb2749b9bc0490182f3401bde9d000876
dianaarutcheva <20106796@mail.wit.ie>
2026-09-24 20:02:41 +0100

    Complete cafe TypeScript

--- src/cafe.ts
+++ src/cafe.ts
@@ -170,6 +170,6 @@ console.log(allergyCard(brownie));
 
 // TS: Three more lines below are bugs that only the compiler can see. Once
 //     your types are in place, fix each one and note it in your commit message.
-console.log(describe(lunchCombo));
-console.log(updateItem(soup, { price: "7.00" }));
-console.log(firstMatch(menu, (i) => i.calories < 300));
+console.log(describe(brownie));
+console.log(updateItem(soup, { price: 7.00 }));
+console.log(firstMatch(menu, (i) => i.nutrition.calories < 300));
